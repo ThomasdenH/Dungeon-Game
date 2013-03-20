8659839adbc7472f696b715c7ae2b93b19bbe7be
Thomas den Hollander <denhollander.thomas@gmail.com>
2013-03-20 19:41:30 +0100

    Added Game, Gamehandler and TileChecker

---
+++ Dungeon-Gamef/src/com/thomas/dungeon/GameTechnical/Game.java
@@ -0,0 +1,302 @@
+/*     */ package com.thomas.dungeon.GameTechnical;
+/*     */ 
+/*     */ import com.thomas.dungeon.Entities.Actor;
+/*     */ import com.thomas.dungeon.Entities.Location;
+/*     */ import com.thomas.dungeon.Entities.Player;
+/*     */ import com.thomas.dungeon.Inventory.Inventory;
+/*     */ import com.thomas.dungeon.Level.Deuren;
+/*     */ import com.thomas.dungeon.Level.plaatsVoorDeur;
+/*     */ import com.thomas.dungeon.ScreenStuff.ContentPane;
+/*     */ import com.thomas.dungeon.ScreenStuff.LevelPrint;
+/*     */ import com.thomas.dungeon.ScreenStuff.Menu;
+/*     */ import com.thomas.dungeon.ScreenStuff.Screenprinting;
+/*     */ import com.thomas.dungeon.armrupgrades.ItemLevels;
+/*     */ import com.thomas.dungeon.carrying.Crafting;
+/*     */ import java.awt.BorderLayout;
+/*     */ import java.awt.Canvas;
+/*     */ import java.awt.Dimension;
+/*     */ import java.awt.Toolkit;
+/*     */ import java.awt.event.KeyEvent;
+/*     */ import java.awt.event.KeyListener;
+/*     */ import java.awt.event.MouseEvent;
+/*     */ import java.awt.event.MouseListener;
+/*     */ import java.util.Random;
+/*     */ import javax.swing.JFrame;
+/*     */ 
+/*     */ public class Game extends Canvas
+/*     */   implements Runnable
+/*     */ {
+/*     */   private static final long serialVersionUID = 2159972266342698357L;
+/*  33 */   public static final Toolkit tool = Toolkit.getDefaultToolkit();
+/*  34 */   public static final Dimension dim = tool.getScreenSize();
+/*  35 */   public static final int HEIGHT = dim.height - dim.height / 10;
+/*  36 */   public static final int WIDTH = dim.width - dim.width / 10;
+/*     */   public static final int framesPerSeconde = 25;
+/*  38 */   public Actor pl1 = new Player();
+/*  39 */   public static Random rand = new Random();
+/*     */ 
+/*  41 */   public boolean running = false;
+/*     */   public static JFrame frame;
+/*  68 */   public static LevelPrint l = new LevelPrint();
+/*  69 */   public static ContentPane cp = new ContentPane();
+/*     */ 
+/*  71 */   public static Menu menu = new Menu();
+/*     */ 
+/*  73 */   public static Screenprinting scre = new Screenprinting();
+/*     */ 
+/*  75 */   public static String[][] finalMap = new String[80][50];
+/*     */   public static String[][] finalTorches;
+/*     */   public static int[][] finalDark;
+/*     */   public static String[][] finalVariations;
+/*  81 */   public static plaatsVoorDeur[] EntityThings = new plaatsVoorDeur[4080];
+/*     */ 
+/*  83 */   public static Player pla = Player.getPlayer();
+/*     */ 
+/*  85 */   public static double FRAMES_PER_SECOND = 25.0D;
+/*     */ 
+/*  87 */   public static double SKIP_TICKS = 1000.0D / FRAMES_PER_SECOND;
+/*     */ 
+/*  89 */   public static double sleep_time = 0.0D;
+/*     */ 
+/*  91 */   public static boolean game_is_running = true;
+/*     */   public static double tickbegin;
+/*     */   public static double ticknow;
+/*     */   public static double next_game_tick;
+/*     */   public static int StairLocationX;
+/*     */   public static int StairLocationY;
+/*     */ 
+/*     */   public static void EntitySet()
+/*     */   {
+/*  99 */     Deuren.DeurenSettings();
+/* 100 */     for (int x = 0; x < 80; x++)
+/* 101 */       for (int y = 0; y < 50; y++) {
+/* 102 */         if (finalMap[x][y] == "D") {
+/* 103 */           Deuren.setDoor(x, y, 1);
+/*     */         }
+/* 105 */         if (finalMap[x][y] == "B")
+/* 106 */           Deuren.setDoor(x, y, 2);
+/*     */       }
+/*     */   }
+/*     */ 
+/*     */   public static synchronized void Inittialize()
+/*     */   {
+/* 115 */     cp.setFocusable(true);
+/* 116 */     cp.addKeyListener(new KeyListener()
+/*     */     {
+/*     */       public void keyPressed(KeyEvent e) {
+/* 119 */         if (e.getKeyCode() == 68) {
+/* 120 */           if (Gamehandler.menu) {
+/* 121 */             Menu.buttonToTheRight();
+/* 122 */           } else if (Gamehandler.singlePlayerGame) {
+/* 123 */             if (!Gamehandler.inGameMenu) {
+/* 124 */               Player.getPlayer().goRight();
+/*     */             }
+/* 126 */             if (Gamehandler.inGameMenu) {
+/* 127 */               Gamehandler.inGameMenuRight();
+/*     */             }
+/*     */           }
+/*     */         }
+/*     */ 
+/* 132 */         if (e.getKeyCode() == 65) {
+/* 133 */           if (Gamehandler.menu) {
+/* 134 */             Menu.buttonToTheLeft();
+/* 135 */           } else if (Gamehandler.singlePlayerGame) {
+/* 136 */             if (!Gamehandler.inGameMenu) {
+/* 137 */               Player.getPlayer().goLeft();
+/*     */             }
+/* 139 */             if (Gamehandler.inGameMenu) {
+/* 140 */               Gamehandler.inGameMenuLeft();
+/*     */             }
+/*     */           }
+/*     */         }
+/*     */ 
+/* 145 */         if ((e.getKeyCode() == 87) && 
+/* 146 */           (!Gamehandler.menu))
+/*     */         {
+/* 148 */           if (Gamehandler.singlePlayerGame) {
+/* 149 */             if (!Gamehandler.inGameMenu) {
+/* 150 */               Player.getPlayer().goUp();
+/*     */             }
+/* 152 */             if (Gamehandler.inGameMenu) {
+/* 153 */               Gamehandler.inGameMenuUp();
+/*     */             }
+/*     */           }
+/*     */         }
+/*     */ 
+/* 158 */         if ((e.getKeyCode() == 83) && 
+/* 159 */           (!Gamehandler.menu))
+/*     */         {
+/* 161 */           if (Gamehandler.singlePlayerGame) {
+/* 162 */             if (!Gamehandler.inGameMenu) {
+/* 163 */               Player.getPlayer().goDown();
+/*     */             }
+/* 165 */             if (Gamehandler.inGameMenu) {
+/* 166 */               Gamehandler.inGameMenuDown();
+/*     */             }
+/*     */           }
+/*     */         }
+/*     */ 
+/* 171 */         if (e.getKeyCode() == 76) {
+/* 172 */           if (Gamehandler.menu) {
+/* 173 */             Menu.buttonClicked();
+/* 174 */           } else if (Gamehandler.singlePlayerGame) {
+/* 175 */             if (!Gamehandler.inGameMenu) {
+/* 176 */               Game.pla.interact();
+/*     */             }
+/* 178 */             if (Gamehandler.inGameMenu) {
+/* 179 */               switch (Gamehandler.inventoryType) {
+/*     */               case 1:
+/* 181 */                 ItemLevels.buyUpgrade();
+/* 182 */                 break;
+/*     */               case 2:
+/* 184 */                 Crafting.buyItems(Gamehandler.crafting + Gamehandler.crafting3);
+/*     */               }
+/*     */             }
+/*     */           }
+/*     */ 
+/*     */         }
+/*     */ 
+/* 191 */         if (e.getKeyCode() == 75) {
+/* 192 */           if (Gamehandler.menu)
+/* 193 */             Menu.buttonClicked();
+/* 194 */           else if ((Gamehandler.singlePlayerGame) && 
+/* 195 */             (!Gamehandler.inGameMenu)) {
+/* 196 */             Game.pla.hit();
+/*     */           }
+/*     */ 
+/*     */         }
+/*     */ 
+/* 201 */         if ((e.getKeyCode() == 77) && 
+/* 202 */           (Gamehandler.inGameMenuAble)) {
+/* 203 */           Gamehandler.setInGameMenu(1);
+/*     */         }
+/*     */ 
+/* 207 */         if ((e.getKeyCode() == 73) && 
+/* 208 */           (Gamehandler.inGameMenuAble)) {
+/* 209 */           Gamehandler.setInGameMenu(2);
+/*     */         }
+/*     */ 
+/* 214 */         if (e.getKeyCode() == 32) {
+/* 215 */           Game.pla.skipTurn();
+/*     */         }
+/* 217 */         if (e.getKeyCode() == 27) {
+/* 218 */           Gamehandler.goBackToMenu();
+/*     */         }
+/* 220 */         if (e.getKeyCode() == 72) {
+/* 221 */           Player.getPlayer().Heal(10000000);
+/* 222 */           Inventory.addAmountOfItem(Game.rand.nextInt(3), 1);
+/*     */         }
+/*     */       }
+/*     */ 
+/*     */       public void keyReleased(KeyEvent e)
+/*     */       {
+/*     */       }
+/*     */ 
+/*     */       public void keyTyped(KeyEvent e)
+/*     */       {
+/*     */       }
+/*     */     });
+/* 234 */     cp.addMouseListener(new MouseListener()
+/*     */     {
+/*     */       public void mouseClicked(MouseEvent arg0)
+/*     */       {
+/*     */       }
+/*     */ 
+/*     */       public void mouseEntered(MouseEvent arg0)
+/*     */       {
+/*     */       }
+/*     */ 
+/*     */       public void mouseExited(MouseEvent arg0)
+/*     */       {
+/*     */       }
+/*     */ 
+/*     */       public void mousePressed(MouseEvent arg0)
+/*     */       {
+/*     */       }
+/*     */ 
+/*     */       public void mouseReleased(MouseEvent arg0)
+/*     */       {
+/*     */       }
+/*     */     });
+/* 253 */     cp.requestFocus();
+/*     */ 
+/* 255 */     Gamehandler.updateLevel();
+/*     */ 
+/* 257 */     pla.loc.setX(39);
+/* 258 */     pla.loc.setY(25);
+/*     */ 
+/* 260 */     scre.printMap();
+/*     */ 
+/* 262 */     EntitySet();
+/*     */   }
+/*     */ 
+/*     */   public static void main(String[] args)
+/*     */   {
+/* 268 */     Game game = new Game();
+/*     */ 
+/* 270 */     frame = new JFrame("Title");
+/* 271 */     frame.setDefaultCloseOperation(3);
+/* 272 */     frame.setLayout(new BorderLayout());
+/* 273 */     frame.add(cp, "Center");
+/* 274 */     frame.setResizable(false);
+/* 275 */     frame.pack();
+/* 276 */     frame.setVisible(true);
+/* 277 */     frame.setLocationRelativeTo(null);
+/*     */ 
+/* 280 */     Inittialize();
+/* 281 */     game.start();
+/*     */   }
+/*     */ 
+/*     */   public void run()
+/*     */   {
+/* 290 */     while (this.running) {
+/* 291 */       tick();
+/* 292 */       runStep();
+/*     */     }
+/*     */   }
+/*     */ 
+/*     */   public void runStep()
+/*     */   {
+/* 301 */     ticknow = System.currentTimeMillis();
+/* 302 */     if (ticknow - tickbegin > SKIP_TICKS)
+/*     */     {
+/* 304 */       cp.repaint();
+/* 305 */       tickbegin = System.currentTimeMillis();
+/*     */     } else {
+/* 307 */       sleep_time = ticknow - tickbegin;
+/*     */       try {
+/* 309 */         Thread.sleep((long) sleep_time);
+/*     */       }
+/*     */       catch (InterruptedException e) {
+/* 312 */         e.printStackTrace();
+/*     */       }
+/*     */     }
+/*     */     try {
+/* 316 */       Thread.sleep(20L);
+/*     */     }
+/*     */     catch (InterruptedException e) {
+/* 319 */       e.printStackTrace();
+/*     */     }
+/*     */   }
+/*     */ 
+/*     */   public void start()
+/*     */   {
+/* 326 */     this.running = true;
+/* 327 */     tickbegin = System.currentTimeMillis();
+/*     */ 
+/* 329 */     new Thread(this).start();
+/*     */   }
+/*     */ 
+/*     */   public void stop() {
+/* 333 */     this.running = false;
+/*     */   }
+/*     */ 
+/*     */   public void tick() {
+/* 337 */     cp.repaint();
+/*     */   }
+/*     */ }
+
+/* Location:           C:\Users\Thomas\Dropbox\Java source\RPG\RPGGame.jar
+ * Qualified Name:     com.thomas.dungeon.GameTechnical.Game
+ * JD-Core Version:    0.6.2
+ */
---
+++ Dungeon-Gamef/src/com/thomas/dungeon/GameTechnical/TileChecker.java
@@ -0,0 +1,295 @@
+/*     */ package com.thomas.dungeon.GameTechnical;
+/*     */ 
+/*     */ import com.thomas.dungeon.Entities.Location;
+/*     */ import com.thomas.dungeon.Entities.Mob;
+/*     */ import com.thomas.dungeon.Entities.Player;
+/*     */ import com.thomas.dungeon.Items.Chest;
+/*     */ import com.thomas.dungeon.Level.Deur;
+/*     */ import com.thomas.dungeon.Level.Deuren;
+/*     */ import java.io.PrintStream;
+/*     */ import java.util.Random;
+/*     */ 
+/*     */ public class TileChecker
+/*     */ {
+/*  12 */   static Random rand = new Random();
+/*     */   public static int locRelativeToPlayerX;
+/*     */   public static int locRelativeToPlayerY;
+/*     */   public static boolean randomMove;
+/*     */   public static int direction;
+/*     */   public static int XDirection;
+/*     */   public static int YDirection;
+/*     */   static int amountAround;
+/*     */ 
+/*     */   public static int directionIfNearPlayer(int LocationX, int LocationY)
+/*     */   {
+/*  25 */     if (Player.getPlayer().loc.getX() < LocationX)
+/*  26 */       XDirection = -1;
+/*  27 */     else if (Player.getPlayer().loc.getX() > LocationX)
+/*  28 */       XDirection = 1;
+/*     */     else {
+/*  30 */       XDirection = 0;
+/*     */     }
+/*     */ 
+/*  33 */     if (Player.getPlayer().loc.getY() < LocationY)
+/*  34 */       YDirection = -1;
+/*  35 */     else if (Player.getPlayer().loc.getY() > LocationY)
+/*  36 */       YDirection = 1;
+/*     */     else {
+/*  38 */       YDirection = 0;
+/*     */     }
+/*     */ 
+/*  41 */     if (XDirection == 0)
+/*  42 */       return Converter.convertYDirectionToDirection(YDirection);
+/*  43 */     if (YDirection == 0) {
+/*  44 */       return Converter.convertXDirectionToDirection(XDirection);
+/*     */     }
+/*  46 */     if (rand.nextBoolean()) {
+/*  47 */       return Converter.convertYDirectionToDirection(YDirection);
+/*     */     }
+/*  49 */     return Converter.convertXDirectionToDirection(XDirection);
+/*     */   }
+/*     */ 
+/*     */   public static boolean equalsUp(int x, int y)
+/*     */   {
+/*  56 */     if (Game.finalMap[x][y].equals("X")) {
+/*  57 */       return true;
+/*     */     }
+/*  59 */     if (Game.finalMap[x][y].equals("x")) {
+/*  60 */       return true;
+/*     */     }
+/*  62 */     return false;
+/*     */   }
+/*     */ 
+/*     */   public static boolean isWalkable(int act, int LocX, int LocY) {
+/*  66 */     if ((LocX < 0) || (LocX >= 80) || (LocY < 0) || (LocY >= 50)) {
+/*  67 */       return false;
+/*     */     }
+/*     */ 
+/*  70 */     switch (act)
+/*     */     {
+/*     */     case 0:
+/*  73 */       break;
+/*     */     case 1:
+/*  76 */       if (com.thomas.dungeon.Entities.Mobs.mob[(LocX + LocY * 80)].isMob) {
+/*  77 */         return false;
+/*     */       }
+/*  79 */       if (com.thomas.dungeon.Items.Chests.chestst[(LocX + LocY * 80)].isChest) {
+/*  80 */         return false;
+/*     */       }
+/*  82 */       if (Game.finalMap[LocX][LocY] == "-") {
+/*  83 */         return true;
+/*     */       }
+/*  85 */       if (Deuren.getSingleDoor(LocX, LocY).open) {
+/*  86 */         return true;
+/*     */       }
+/*     */ 
+/*     */       break;
+/*     */     case 2:
+/*  93 */       if ((LocX == Player.getPlayer().loc.getX()) && (LocY == Player.getPlayer().loc.getY())) {
+/*  94 */         return false;
+/*     */       }
+/*  96 */       if (com.thomas.dungeon.Entities.Mobs.mob[(LocX + LocY * 80)].isMob) {
+/*  97 */         return false;
+/*     */       }
+/*  99 */       if (com.thomas.dungeon.Items.Chests.chestst[(LocX + LocY * 80)].isChest) {
+/* 100 */         return false;
+/*     */       }
+/* 102 */       if (Game.finalMap[LocX][LocY] == "-") {
+/* 103 */         return true;
+/*     */       }
+/* 105 */       if (Deuren.getSingleDoor(LocX, LocY).open) {
+/* 106 */         return true;
+/*     */       }
+/*     */ 
+/*     */       break;
+/*     */     case 3:
+/* 111 */       if ((LocX == Player.getPlayer().loc.getX()) && (LocY == Player.getPlayer().loc.getY())) {
+/* 112 */         return false;
+/*     */       }
+/* 114 */       if (com.thomas.dungeon.Entities.Mobs.mob[(LocX + LocY * 80)].isMob) {
+/* 115 */         return false;
+/*     */       }
+/* 117 */       if (com.thomas.dungeon.Items.Chests.chestst[(LocX + LocY * 80)].isChest) {
+/* 118 */         return false;
+/*     */       }
+/* 120 */       if (Game.finalMap[LocX][LocY] == "-") {
+/* 121 */         return true;
+/*     */       }
+/* 123 */       if (Deuren.getIfDoor(LocX, LocY)) {
+/* 124 */         return true;
+/*     */       }
+/*     */       break;
+/*     */     case 4:
+/* 128 */       if ((LocX == Player.getPlayer().loc.getX()) && (LocY == Player.getPlayer().loc.getY())) {
+/* 129 */         return false;
+/*     */       }
+/* 131 */       if (com.thomas.dungeon.Entities.Mobs.mob[(LocX + LocY * 80)].isMob) {
+/* 132 */         return false;
+/*     */       }
+/* 134 */       if (com.thomas.dungeon.Items.Chests.chestst[(LocX + LocY * 80)].isChest) {
+/* 135 */         return false;
+/*     */       }
+/* 137 */       if (Game.finalMap[LocX][LocY] == "-") {
+/* 138 */         return true;
+/*     */       }
+/* 140 */       if (Deuren.getSingleDoor(LocX, LocY).open) {
+/* 141 */         return true;
+/*     */       }
+/*     */       break;
+/*     */     case 5:
+/* 145 */       if ((LocX == Player.getPlayer().loc.getX()) && (LocY == Player.getPlayer().loc.getY())) {
+/* 146 */         return false;
+/*     */       }
+/* 148 */       if (com.thomas.dungeon.Entities.Mobs.mob[(LocX + LocY * 80)].isMob) {
+/* 149 */         return false;
+/*     */       }
+/* 151 */       if (com.thomas.dungeon.Items.Chests.chestst[(LocX + LocY * 80)].isChest) {
+/* 152 */         return false;
+/*     */       }
+/* 154 */       if (Game.finalMap[LocX][LocY] == "-") {
+/* 155 */         return true;
+/*     */       }
+/* 157 */       if (Deuren.getSingleDoor(LocX, LocY).open) {
+/* 158 */         return true;
+/*     */       }
+/*     */       break;
+/*     */     default:
+/* 162 */       System.out.println("TileChecker.walkable error");
+/*     */     }
+/*     */ 
+/* 166 */     return false;
+/*     */   }
+/*     */ 
+/*     */   public static boolean isWalkableDirection(int actor, int LocX, int LocY, int direction) {
+/* 170 */     switch (direction) {
+/*     */     case 0:
+/* 172 */       return isWalkable(actor, LocX, LocY - 1);
+/*     */     case 1:
+/* 174 */       return isWalkable(actor, LocX + 1, LocY);
+/*     */     case 2:
+/* 176 */       return isWalkable(actor, LocX, LocY + 1);
+/*     */     case 3:
+/* 178 */       return isWalkable(actor, LocX - 1, LocY);
+/*     */     }
+/* 180 */     return true;
+/*     */   }
+/*     */ 
+/*     */   public static boolean isNextToPlayer(int LocationX, int LocationY)
+/*     */   {
+/* 186 */     if ((LocationX + 1 == Player.getPlayer().loc.getX()) && (LocationY == Player.getPlayer().loc.getY())) {
+/* 187 */       return true;
+/*     */     }
+/* 189 */     if ((LocationX - 1 == Player.getPlayer().loc.getX()) && (LocationY == Player.getPlayer().loc.getY())) {
+/* 190 */       return true;
+/*     */     }
+/* 192 */     if ((LocationX == Player.getPlayer().loc.getX()) && (LocationY + 1 == Player.getPlayer().loc.getY())) {
+/* 193 */       return true;
+/*     */     }
+/* 195 */     if ((LocationX == Player.getPlayer().loc.getX()) && (LocationY - 1 == Player.getPlayer().loc.getY())) {
+/* 196 */       return true;
+/*     */     }
+/* 198 */     return false;
+/*     */   }
+/*     */ 
+/*     */   public static boolean tileAround(int LocX, int LocY, String tile, int direction)
+/*     */   {
+/* 204 */     switch (direction) {
+/*     */     case 0:
+/*     */       try {
+/* 207 */         if (Game.finalMap[LocX][(LocY - 1)].equals(tile))
+/* 208 */           return true;
+/*     */       }
+/*     */       catch (Exception e)
+/*     */       {
+/* 212 */         return false;
+/*     */       }
+/*     */     case 1:
+/*     */       try {
+/* 216 */         if (Game.finalMap[(LocX + 1)][LocY].equals(tile))
+/* 217 */           return true;
+/*     */       }
+/*     */       catch (Exception e)
+/*     */       {
+/* 221 */         return false;
+/*     */       }
+/*     */     case 2:
+/*     */       try {
+/* 225 */         if (Game.finalMap[LocX][(LocY + 1)].equals(tile))
+/* 226 */           return true;
+/*     */       }
+/*     */       catch (Exception e)
+/*     */       {
+/* 230 */         return false;
+/*     */       }
+/*     */     case 3:
+/*     */       try {
+/* 234 */         if (Game.finalMap[(LocX - 1)][LocY].equals(tile))
+/* 235 */           return true;
+/*     */       }
+/*     */       catch (Exception e)
+/*     */       {
+/* 239 */         return false;
+/*     */       }
+/*     */     }
+/*     */ 
+/* 243 */     return false;
+/*     */   }
+/*     */ 
+/*     */   public static int amountTileAround(int LocX, int LocY, String tile)
+/*     */   {
+/* 249 */     amountAround = 0;
+/* 250 */     for (int e = 0; e < 4; e++) {
+/* 251 */       if (tileAround(e, e, tile, e)) {
+/* 252 */         amountAround += 1;
+/*     */       }
+/*     */     }
+/* 255 */     return amountAround;
+/*     */   }
+/*     */ 
+/*     */   public static boolean tileAround(int LocX, int LocY, String tile, int direction, String[][] map)
+/*     */   {
+/*     */     try {
+/* 261 */       switch (direction) {
+/*     */       case 0:
+/* 263 */         if (map[LocX][(LocY - 1)].equals(tile)) {
+/* 264 */           return true;
+/*     */         }
+/*     */         break;
+/*     */       case 1:
+/* 268 */         if (map[(LocX + 1)][LocY].equals(tile)) {
+/* 269 */           return true;
+/*     */         }
+/*     */         break;
+/*     */       case 2:
+/* 273 */         if (map[LocX][(LocY + 1)].equals(tile)) {
+/* 274 */           return true;
+/*     */         }
+/*     */         break;
+/*     */       case 3:
+/* 278 */         if (map[(LocX - 1)][LocY].equals(tile)) {
+/* 279 */           return true;
+/*     */         }
+/*     */         break;
+/*     */       }
+/*     */     }
+/*     */     catch (Exception localException)
+/*     */     {
+/*     */     }
+/* 287 */     return false;
+/*     */   }
+/*     */ 
+/*     */   public static int amountTileAround(int LocX, int LocY, String tile, String[][] map) {
+/* 291 */     amountAround = 0;
+/* 292 */     for (int e = 0; e < 4; e++) {
+/* 293 */       if (tileAround(LocX, LocY, tile, e, map)) {
+/* 294 */         amountAround += 1;
+/*     */       }
+/*     */     }
+/* 297 */     return amountAround;
+/*     */   }
+/*     */ }
+
+/* Location:           C:\Users\Thomas\Dropbox\Java source\RPG\RPGGame.jar
+ * Qualified Name:     com.thomas.dungeon.GameTechnical.TileChecker
+ * JD-Core Version:    0.6.2
+ */
